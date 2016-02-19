a66db5139a660afed9bf1e6f9ca8e3ba438afc91
jsyczhanghao <jsyczhanghao@sina.com>
2016-02-19 14:05:23 +0800

    commit

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = function(feather, opts){	
+	var moduleName = feather.config.get('project.modulename');
+
+	if(moduleName == 'common'){
+		var DIR = (feather.config.env().get('component.dir') || 'components/').replace(/\/$/, '');
+
+		feather.on('lookup:file', function(info, file){
+			if(!info.file){
+				info._rest = info.rest;
+			}
+		});
+
+		require('fis3-hook-components')(feather, opts);
+		
+		feather.on('lookup:file', function(info, file){
+			if(info.file && info.file.isComponent && info._rest){
+				feather.commonInfo.components[info._rest] = {
+					dir: DIR + '/' + info._rest,
+					fullName: info.file.id
+				};
+			}
+		});
+	}else if(moduleName){
+		var RULE = /^([0-9a-zA-Z\.\-_]+)(?:\/(.+))?$/;
+		var componentInfo = feather.commonInfo.components, map = feather.commonInfo.map;
+
+		require('fis3-hook-components')(feather, opts);
+
+		feather.on('lookup:file', function(info, file){
+			if(!info.file){
+
+				var fullName = info.rest
+
+				if(!map[fullName]){
+					var match = RULE.exec(info.rest);
+
+					if(match){
+						var cName = match[1], basename = match[2], component;
+
+						if(component = componentInfo[cName]){
+							fullName = basename ? component.dir + '/' + basename : component.fullName;
+						}
+
+						info.rest = fullName;
+					}
+				}
+
+				if(map[fullName]){
+					var resolved = info.file = feather.file.wrap(fullName);
+					resolved.url = '/' + fullName;
+					resolved.id = fullName;
+					resolved.setContent('');
+					resolved.useHash = false;
+					resolved.useDomain = false;
+					resolved.useMap = false;
+					resolved.useCompile = false;
+					resolved.useCache = false;
+					resolved.getUrl = function(){
+						return resolved.url;
+					};
+					resolved.isFile = function(){
+						return true;
+					};
+				}
+			}
+		});
+	}
+};
