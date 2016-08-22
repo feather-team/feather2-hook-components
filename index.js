1b1e371cd8dd956c49baac5f2d7bac042ea74826
zhanghao <jsyczhanghao@sina.com>
2016-08-22 15:09:35 +0800

    commit

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
-'use strict';
-
 var path = require('path');
+var componentsInfo, componentsDir;
 
-function _findResource(name, map){
-	if(map[name]){
-		return name;
+function onReleaseStart(){
+	// 读取组件信息
+	componentsInfo = {};
+	componentsDir = (feather.config.env().get('component.dir') || 'components/').replace(/\/$/, '');
+
+	if(componentsDir.substr(-1) !== '/'){
+		componentsDir += '/';
 	}
 
-	var extList = ['.js', '.jsx', '.coffee', '.css', '.sass', '.scss', '.less', '.html', '.tpl', '.vm'];
+	var files = feather.project.getSourceByPatterns(componentsDir + '**/bower.json');
 
-	for(var i = 0, len = extList.length; i < len; i++){
-		var tmpName = name + extList[i];
+	Object.keys(files).forEach(function(subpath){
+		var file = files[subpath];
+		var cName = path.basename(path.dirname(subpath));
+		var json;
 
-		if(map[tmpName]){
-			return tmpName;
+		try{
+			json = JSON.parse(file.getContent());
+		}catch(e){
+			feather.log.warn('unable to load bower.json of component `%s`.', cName);
 		}
-	}
 
-	return;
+		if(json.hook && typeof json.hook === 'string'){
+			try{
+				var root = feather.project.getProjectPath();
+				var hook = require(path.join(root, componentsDir, cName, json.hook));
+				hook && hook(json, files);
+			}catch(e){
+				// don't care
+			}
+		}
+
+		json.name = json.name || cName;
+		componentsInfo[json.name] = json;
+	});
+
+	feather.emit('components:info', componentsInfo);
 }
 
-function findResource(name, map){
-	var info, list = [
+function _findResource(name, path){
+	var extList = feather.config.get('component.ext', []);
+	var info = feather.uri(name, path);
+
+	for(var i = 0, len = extList.length; i < len && !info.file; i++){
+		info = feather.uri(name + extList[i], path);
+	}
+
+	return info;
+}
+
+function findResource(name, dir){
+	var list = [
 		name,
-		name + '/index',
-		name + '/' + path.basename(name)
+		path.join(name, 'index'),
+		path.join(name, path.basename(name))
 	];
+	var info;
 
 	while(list.length){
 		name = list.shift();
+		info = _findResource(name, dir);
 
-		if(info = _findResource(name, map)){
+		if(info && info.file){
 			break;
 		}
 	}
@@ -38,79 +71,39 @@ function findResource(name, map){
 	return info;
 }
 
-module.exports = function(feather, opts){	
-	var moduleName = feather.config.get('project.modulename');
-
-	require('fis3-hook-components')(feather, opts);
-
-	if(moduleName == 'common'){
-		feather.on('components:info', function(info){
-			feather.releaseInfo.components = info;
-		});
-
-		// feather.on('lookup:file', function(info, file){
-		// 	if(!info.file){
-		// 		info._rest = info.rest;
-		// 	}
-		// });
-
-		
-		
-		// feather.on('lookup:file', function(info, file){
-		// 	if(info.file && info.file.isComponent && info._rest){
-		// 		feather.commonInfo.components[info._rest] = {
-		// 			dir: DIR + '/' + info._rest,
-		// 			fullName: info.file.id
-		// 		};
-		// 	}
-		// });
-	}else if(moduleName){
-		var RULE = /^([0-9a-zA-Z\.\-_]+)(?:\/(.+))?\/?$/;
-		var DIR = (feather.config.env().get('component.dir') || 'components/').replace(/^\/+|\/+$/, '');
-		var componentInfo = feather.releaseInfo.components, map = feather.releaseInfo.map;
-
-		feather.on('lookup:file', function(info, file){
-			if(!info.file){
-				var fullName = info.rest;
-
-				if(!map[fullName]){
-					var match = RULE.exec(info.rest);
-
-					if(match){
-						var cName = match[1], subpath = match[2], config = componentInfo[cName] || {};
-
-						if(subpath){
-							fullName = findResource(DIR + '/' + cName + '/' + subpath, map);
-						}else{
-							fullName = findResource(DIR + '/' + cName + '/' + (config.main || 'index'), map);
-
-							if(!fullName){
-								fullName = findResource(DIR + '/' + cName + '/' + cName, map);
-							}
-						}
-					}
-				}
-
-				if(fullName && map[fullName]){
-					var resolved = info.file = feather.file.wrap(fullName);
-					resolved.url = '/' + fullName;
-					resolved.id = fullName;
-					resolved.setContent('');
-					resolved.useHash = false;
-					resolved.useDomain = false;
-					resolved.useMap = false;
-					resolved.useCompile = false;
-					resolved.useCache = false;
-					resolved.getUrl = function(){
-						return resolved.url;
-					};
-					resolved.isFile = function(){
-						return true;
-					};
-
-					info.rest = fullName;
-				}
-			}
-		});
+function onFileLookUp(info, file){
+	// 如果已经找到了，没必要再找了。
+	if(info.file || file && file.useShortPath === false){
+		return;
 	}
+
+	var m = /^([0-9a-zA-Z\.\-_]+)(?:\/(.+))?$/.exec(info.rest);
+	if(m){
+		var cName = m[1];
+		var subpath = m[2];
+		var config = componentsInfo[cName] ||{};
+		var resolved;
+
+		if(subpath){
+			resolved = findResource('/' + componentsDir + cName + '/' + subpath, file ? file.dirname : feather.project.getProjectPath());
+		}else{
+			resolved = findResource('/' + componentsDir + cName + '/' +(config.main || 'index'), file ? file.dirname : feather.project.getProjectPath());
+
+			if(!resolved.file){
+				resolved = findResource('/' + componentsDir + cName + '/' + cName, file ? file.dirname : feather.project.getProjectPath());
+			}
+		}
+
+		// 根据规则找到了。
+		if(resolved.file){
+			info.id = resolved.file.getId();
+			info.file = resolved.file;
+		}
+	}
+}
+
+
+module.exports = function(feather, opts){
+	feather.on('release:start', onReleaseStart);
+	feather.on('lookup:file', onFileLookUp);
 };
